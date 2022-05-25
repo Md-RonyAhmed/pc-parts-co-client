00617b25863b06ee6a2a717fa560aa2d5e465b24
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-26 03:12:36 +0600

    Added blogs and blog

---
+++ src/Pages/Blog/Blog.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+const Blog = ({ blog }) => {
+   const { name, img, desc } = blog;
+   return (
+     <div className="flex flex-wrap mb-12">
+       <div className="grow-0 shrink-0 basis-auto w-full lg:w-6/12 lg:pr-6 mb-6 lg:mb-0">
+         <div
+           className="relative overflow-hidden bg-no-repeat bg-cover ripple shadow-lg rounded-lg"
+           style={{ backgroundPosition: `${50}%` }}
+           data-mdb-ripple="true"
+           data-mdb-ripple-color="light"
+         >
+           <img src={img} className="w-full" alt="Louvre" />
+           <a href="#!">
+             <div
+               className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
+               style={{ backgroundColor: `rgba(251, 251, 251, 0.2)` }}
+             ></div>
+           </a>
+         </div>
+       </div>
+
+       <div className="grow-0 shrink-0 basis-auto w-full lg:w-6/12 mb-6 lg:pl-6">
+            <h3 className="text-2xl font-bold mb-2">{ name}</h3>
+         <div className="text-red-600 text-sm mb-2 flex items-center font-medium">
+           Web Technology
+         </div>
+         <p className="text-gray-500 mb-4">
+          {desc.slice(0,280)}
+           <span className="text-blue-500 text-lg ml-1">
+             ...
+           </span>
+         </p>
+         <div className="w-36 mt-2">
+           <Link
+             to="/blogs/details"
+             type="button"
+             className="btn btn-outline btn-info"
+           >
+             Read more...
+           </Link>
+         </div>
+       </div>
+     </div>
+   );
+};
+
+export default Blog;
--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import { toast } from 'react-toastify';
+import Blog from '../Blog/Blog';
+import Loading from '../Shared/Loading';
+
 
 const Blogs = () => {
-   return (
-      <div>
-         <h1>Blogs</h1>
+  const [blogs, setBlogs] = useState([]);
+  const url = `http://localhost:5000/blogs`;
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get(url);
+      if (!data?.success) {
+        setBlogs([]);
+        return toast.error(data.error);
+      }
+      console.log(data?.data)
+      setBlogs(data?.data);
+    })();
+  }, [url]);    
+  
+  return (
+    <div className="container my-24 px-6 mx-auto mt-24">
+      <div className="mb-0 p-1 md:w-3/4 mx-auto">
+        <h1 className="text-4xl font-bold text-center mx-auto text-primary my-6 border-b-4 border-black pb-1 font-mono md:w-1/2 w-5/6">
+          Our Latest Blogs
+        </h1>
       </div>
-   );
+      <section className="mb-32 text-gray-800">
+        {blogs?.length ? (
+          blogs.map((blog) => (
+            <Blog key={blog._id} blog={blog}></Blog>
+          ))
+        ) : (
+          <div className="md:ml-[500px]">
+            <Loading></Loading>
+          </div>
+        )}
+      </section>
+    </div>
+  );
 };
 
 export default Blogs;
