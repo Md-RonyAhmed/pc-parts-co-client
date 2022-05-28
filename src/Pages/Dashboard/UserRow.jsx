b474a320540907cd4a2b1ea22b8f22842796db79
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-28 20:15:02 +0600

    hook added

--- src/Pages/Dashboard/UserRow.jsx
+++ src/Pages/Dashboard/UserRow.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const UserRow = ({ user, refetch,i }) => {
-   const { email,_id } = user;
+   const { email,role, _id } = user;
+   const makeAdmin = () => {
+     fetch(`http://localhost:5000/user/admin/${email}`, {
+       method: "PUT",
+       headers: {
+         authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+       },
+     })
+       .then((res) => {
+         if (res.status === 403) {
+           toast.error("Failed to Make an admin");
+         }
+         return res.json();
+       })
+       .then((data) => {
+         if (data.modifiedCount > 0) {
+           refetch();
+           toast.success(`Successfully made an admin`);
+         }
+       });
+   };
    return (
      <tr>
        <th>{i + 1}</th>
        <td>{_id}</td>
        <td>{email}</td>
        <td>
-         
-           <button class="btn btn-xs">
+         {role !== "admin" && (
+           <button onClick={makeAdmin} class="btn btn-xs">
              Make Admin
            </button>
-         
+         )}
        </td>
        <td>
          <button class="btn btn-xs">Remove User</button>
