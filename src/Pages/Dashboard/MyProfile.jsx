2dbc31ea323769be68373cab2d9b33aea47a21e7
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-28 13:11:30 +0600

    My profile updated completed

--- src/Pages/Dashboard/MyProfile.jsx
+++ src/Pages/Dashboard/MyProfile.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import img from "../../Assets/Images/images.png"
+import axios from 'axios';
+import { toast } from 'react-toastify';
 const MyProfile = () => {
    const [user] = useAuthState(auth);
+   const handleSubmit = async (event) => {
+     event.preventDefault();
+     const updateUser = {
+       userName: event.target.uname.value,
+       email: event.target.email.value,
+       address: event.target.address.value,
+       phone: event.target.phone.value,
+       linkedIn:event.target.linkedin.value
+     };
+     try {
+       const { data } = await axios.put(`http://localhost:5000/userProfile/${user?.email}`, updateUser);
+       if (!data.success) {
+         return toast.error(data.error);
+       }
+
+       toast.success(data.message);
+       event.target.reset();
+     } catch (error) {
+       toast.error(error.message);
+     }
+   };
+  
    return (
      <div>
        <div className="pt-2 w-full mb-10">
@@ -17,41 +41,15 @@ const MyProfile = () => {
                  src={user?.photoURL}
                />
              ) : (
-               <img
-                 width="100px"
-                 alt=""
-                 className="rounded-full"
-                 src={img}
-               />
+               <img width="100px" alt="" className="rounded-full" src={img} />
              )}
            </div>
-           <form>
+           <form onSubmit={handleSubmit}>
              <div className="mx-auto w-full p-4 pl-0">
                <p className=" mb-2 font-bold text-xl text-black">
                  <span className=" text-orange-900"> Name:</span>{" "}
                  {user?.displayName}
                </p>
-               <p className="mb-2">LinkedIn Profile:</p>
-               <input
-                 name="linkedin"
-                 type="text"
-                 required
-                 className="form-control block
-        w-full
-        px-3
-        py-1.5
-        text-base
-        font-normal
-        text-gray-700
-        bg-white bg-clip-padding
-        border border-solid border-gray-300
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                 id="exampleInput90"
-               />
                <p className="mb-2">User Name:</p>
                <input
                  name="uname"
@@ -117,6 +115,28 @@ const MyProfile = () => {
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                  id="exampleInput90"
                  placeholder="Enter your phone"
+               />
+               <p className="mb-2">LinkedIn Profile:</p>
+               <input
+                 name="linkedin"
+                        type="text"
+                        placeholder='Enter LinkedIn profile link'
+                 required
+                 className="form-control block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                 id="exampleInput90"
                />
                <p className="mb-2">Address:</p>
                <textarea
--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -4,7 +4,6 @@ const useToken = (user) => {
   const [token, setToken] = useState("");
   useEffect(() => {
     const email = user?.user?.email;
-    console.log(email)
     const currentUser = { email: email };
     if (email) {
       fetch(`http://localhost:5000/user/${email}`, {
