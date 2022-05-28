725b61222d5499a5566c8717a765af070444572a
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-28 15:32:20 +0600

    React rating and fontawesome added

--- src/Pages/Dashboard/AddReview.jsx
+++ src/Pages/Dashboard/AddReview.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import ReactStars from "react-rating-stars-component";
 const AddReview = () => {
+   const secondExample = {
+     size: 25,
+     count: 5,
+     color: "gray",
+     activeColor: "orange",
+     value: 3.5,
+     a11y: true,
+     isHalf: true,
+     onChange: (newValue) => {
+       console.log(`Example 2: new value is ${newValue}`);
+     },
+   };
    return (
-      <div>
-         <h1>Add a Review</h1>
-      </div>
+     <div>
+       <h1>Add a Review</h1>
+       <h1>Add a Review</h1>
+       <h1>Add a Review</h1>
+       <ReactStars {...secondExample} />
+     </div>
    );
 };
 
